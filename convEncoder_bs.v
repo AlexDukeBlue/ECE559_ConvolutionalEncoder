70c4f15008b1cff1f6a18626254de8d1f6e057cf
AlexanderZapataDuke <35411999+AlexanderZapataDuke@users.noreply.github.com>
2018-10-15 16:02:54 -0400

    updating

--- convEncoder_bs.v
+++ convEncoder_bs.v
@@ -1,15 +1,16 @@
-module convEncoder_bs(clk, reset, blk_size, tail_bits, blk_empty, blk_data, blk_rdreq, fifo0_out, fifo1_out, fifo2_out);
+module convEncoder_bs(clk, reset, blk_size, tail_bits, blk_empty, blk_data, blk_rdreq, dOut, cOut);
 	//If the input block is FIFO, not sure how we get the last six
 	input [7:0] blk_data;
 	input [5:0] tail_bits;
 	input clk, reset, blk_size, blk_empty;
-	output [7:0] fifo0_out, fifo1_out, fifo2_out; 
-	output reg  blk_rdreq; 
+	output [6:0] cOut; 
+	output [2:0] dOut;
+	output blk_rdreq; 
 	
-	localparam small_size = 11'd1056;
+	localparam small_size = 13'd1056;
 	localparam large_size = 13'd6144;
 	
-	reg [13:0]  computation_counter;
+	reg [12:0]  computation_counter;
 	reg c0, c1, c2, c3, c4, c5, c6, instantiate_computation;
 	
 	wire [12:0] size;
@@ -20,6 +21,9 @@ module convEncoder_bs(clk, reset, blk_size, tail_bits, blk_empty, blk_data, blk_
 	assign d0 = c0 ^ c2 ^ c3 ^ c5 ^ c6;
 	assign d1 = c0 ^ c1 ^ c2 ^ c3 ^ c6;
 	assign d2 = c0 ^ c1 ^ c2 ^ c4 ^ c6;
+	assign blk_rdreq = computation_counter % 8;
+	assign cOut = {c0, c1, c2, c3, c4, c5, c6};
+	assign dOut = {d0, d1, d2};
 	
 	//We need to figure out how when to decide to start/continue computation other than what it is above.
 	
@@ -32,8 +36,10 @@ module convEncoder_bs(clk, reset, blk_size, tail_bits, blk_empty, blk_data, blk_
 	always @(posedge clk) 
 	begin
 		blk_rdreq <= 1'b0;
-		if(!reset) begin
-			if(compute_enable && !instantiate_computation)begin
+		if(!reset)
+		begin
+			if(compute_enable && !instantiate_computation)
+			begin
 				//Computation Logic
 				c0 <= blk_data[computation_counter % 8];
 				c1 <= c0;
@@ -42,7 +48,13 @@ module convEncoder_bs(clk, reset, blk_size, tail_bits, blk_empty, blk_data, blk_
 				c4 <= c3;
 				c5 <= c4;
 				c6 <= c5;
-			end else if(instantiate_computation) begin
+				computation_counter <= computation_counter + 13'd1;
+				if((computation_counter % 8) && (!blk_empty))
+				begin
+					blk_rdreq <= 1'b1;
+				end
+			end else if(instantiate_computation)
+			begin
 				//Instantiation Logic
 				//Tail_bits goes from [5 .. 0] => [last bit .. 6th to last bit]
 				//Block bits go from [7 .. 0] => [eigth bit .. first bit]
@@ -53,18 +65,20 @@ module convEncoder_bs(clk, reset, blk_size, tail_bits, blk_empty, blk_data, blk_
 				c4 <= tail_bits[2];
 				c5 <= tail_bits[1];
 				c6 <= tail_bits[0];
+				computation_counter <= computation_counter + 13'd1;
+				instantiate_computation <= 1'b0;
 			end
-			computation_counter <= computation_counter + 14'd1;
-			if(((computation_counter % 8) == 0) && (!blk_empty)) begin 
-				blk_rdreq <= 1'b1;
-			end
-		end else begin 
-			computation_counter <= 0;
+		end else if(reset)
+		begin 
+			computation_counter <= 13'd0;
 			instantiate_computation <= 1'b1;
-			if(!blk_empty) begin
+			if(!blk_empty)
+			begin
 				blk_rdreq <= 1'b1;
 			end
 		end
 	end
 
+	//Need to set up case for what to do if blk_empty is asserted when we need it to not be lmao.
+	
 endmodule
