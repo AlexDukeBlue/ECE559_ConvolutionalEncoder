6127ed54ef4f8d00f0c8a51dedd446002414bb17
AlexanderZapataDuke <35411999+AlexanderZapataDuke@users.noreply.github.com>
2018-10-17 15:52:45 -0400

    Update skeleton.v

--- skeleton.v
+++ skeleton.v
@@ -1,6 +1,6 @@
-module skeleton(clk, reset, fifo_w_data, wrreq, usedw, cOut, dOut);
+module skeleton(clk, reset, blk_ready, fifo_w_data, wrreq, usedw, cOut, dOut);
 input [7:0] fifo_w_data;
-input clk, reset, wrreq;
+input clk, reset, blk_ready, wrreq;
 output [6:0] cOut;
 output [2:0] dOut;
 output usedw;
@@ -13,5 +13,5 @@ wire [2:0] usedw;
 wire blk_empty, blk_rdreq;
 
 fifo test_fifo(clk, fifo_w_data, blk_rdreq, reset, wrreq, blk_empty, blk_data, usedw);
-convEncoder_bs encoder(clk, reset, blk_size, tail_bits, blk_empty, blk_data, blk_rdreq, dOut, cOut);
+convEncoder_bs encoder(clk, reset, blk_ready, blk_size, tail_bits, blk_empty, blk_data, blk_rdreq, dOut, cOut);
 endmodule 
