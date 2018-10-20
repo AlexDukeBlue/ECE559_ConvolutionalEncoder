a312c669eecf3577e89800ae6df95a5225b6edd6
Alexander Zapata <alexander.zapata@duke.edu>
2018-10-20 03:09:50 -0400

    update

--- convEncoder_bs.v
+++ convEncoder_bs.v
@@ -1,47 +1,49 @@
-module convEncoder_bs(clk, reset, blk_ready, blk_size, tail_bits, blk_empty, blk_data, blk_rdreq, dOut, cOut);
+module convEncoder_bs(clk, reset, blk_ready, blk_meta, blk_empty, blk_data, blk_meta_rdreq, blk_data_rdreq, dOut, cOut);
 	//If the input block is FIFO, not sure how we get the last six
-	input [7:0] blk_data;
-	input [5:0] tail_bits;
-	input clk, reset, blk_ready, blk_size, blk_empty;
+	input [7:0] blk_data, blk_meta;
+	input clk, reset, blk_ready, blk_empty;
 	output [6:0] cOut; 
 	output [2:0] dOut;
-	output blk_rdreq; 
+	output blk_meta_rdreq, blk_data_rdreq; 
 	
 	reg c0, c1, c2, c3, c4, c5, c6, compute_enable, instantiate_computation;
 	
-	wire [12:0] computation_counter, large_counter_out;
-	wire [11:0] small_counter_out;
+	wire [12:0] counter_out;
 	wire [2:0] counter_mod;
 	wire d0, d1, d2, on_last_bit_of_input, small_computation_notdone, large_computation_notdone, computation_done;
 
-	assign computation_done = (blk_size) ? !large_computation_notdone : !small_computation_notdone;
-	assign computation_counter = (blk_size) ? large_counter_out : {1'b0, small_counter_out};
-	assign blk_rdreq = on_last_bit_of_input && !blk_empty && compute_enable;
+	assign computation_done = (blk_meta[0]) ? !large_computation_notdone : !small_computation_notdone;
+	assign blk_meta_rdreq = instantiate_computation && !blk_empty;
+	assign blk_data_rdreq = on_last_bit_of_input && !blk_empty && compute_enable;
 	assign d0 = c0 ^ c2 ^ c3 ^ c5 ^ c6;
 	assign d1 = c0 ^ c1 ^ c2 ^ c3 ^ c6;
 	assign d2 = c0 ^ c1 ^ c2 ^ c4 ^ c6;
 	assign cOut = {c0, c1, c2, c3, c4, c5, c6};
 	assign dOut = {d0, d1, d2};
 
-	counter_large_block clb(compute_enable, clk, reset, large_counter_out);
-	counter_small_block csb(compute_enable, clk, reset, small_counter_out);
-	large_counter_compare lcc(large_counter_out, large_computation_notdone);
-	small_counter_compare scc(small_counter_out, small_computation_notdone);
+	counter_block cb((compute_enable && !computation_done), clk, (reset || instantiate_computation), counter_out);
+	large_counter_compare lcc(counter_out, large_computation_notdone);
+	small_counter_compare scc(counter_out[10:0], small_computation_notdone);
 	mod_counter mc(compute_enable, clk, reset, counter_mod);
 	mod_compare compare_mod(counter_mod, on_last_bit_of_input);
 	
 	always @(posedge clk) 
 	begin
-		if(computation_done)
+		if(reset)
+		begin 
+			compute_enable <= 1'b0;
+			instantiate_computation <= 1'b0;
+		end
+		else if(computation_done)
 		begin
 			compute_enable <= 1'b0;
 		end
-		if(blk_ready && !compute_enable)
+		else if(blk_ready && !compute_enable)
 		begin
 			 instantiate_computation <= 1'b1;
 			 compute_enable <= 1'b1;
 		end
-		if(!reset)
+		else
 		begin
 			if(compute_enable && !instantiate_computation)
 			begin
@@ -56,21 +58,18 @@ module convEncoder_bs(clk, reset, blk_ready, blk_size, tail_bits, blk_empty, blk
 			end else if(instantiate_computation)
 			begin
 				//Instantiation Logic
-				//Tail_bits goes from [5 .. 0] => [last bit .. 6th to last bit]
+				//Tail_bits goes from [7 .. 2] => [last bit .. 6th to last bit] in the meta array. 
+				//An arbitrary value separates the tail-bits from the size bit.
 				//Block bits go from [7 .. 0] => [eigth bit .. first bit]
 				c0 <= blk_data[0];
-				c1 <= tail_bits[5];
-				c2 <= tail_bits[4];
-				c3 <= tail_bits[3];
-				c4 <= tail_bits[2];
-				c5 <= tail_bits[1];
-				c6 <= tail_bits[0];
+				c1 <= blk_meta[7];
+				c2 <= blk_meta[6];
+				c3 <= blk_meta[5];
+				c4 <= blk_meta[4];
+				c5 <= blk_meta[3];
+				c6 <= blk_meta[2];
 				instantiate_computation <= 1'b0;
 			end
-		end else if(reset)
-		begin 
-			compute_enable <= 1'b0;
-			instantiate_computation <= 1'b0;
 		end
 	end
 
--- skeleton.v
+++ skeleton.v
@@ -1,17 +1,14 @@
-module skeleton(clk, reset, blk_ready, fifo_w_data, wrreq, usedw, cOut, dOut);
-input [7:0] fifo_w_data;
-input clk, reset, blk_ready, wrreq;
+module skeleton(clk, reset, blk_ready, fifo_w_data, fifo_w_meta, wrreq_data, wrreq_meta, cOut, dOut);
+input [7:0] fifo_w_data, fifo_w_meta;
+input clk, reset, blk_ready, wrreq_data, wrreq_meta;
 output [6:0] cOut;
 output [2:0] dOut;
-output usedw;
 
-localparam blk_size = 1'b0;
-localparam tail_bits = 6'b101010;
+wire [7:0] blk_data, blk_meta;
+wire usedw_data, usedw_meta;
+wire blk_empty_data, blk_empty_meta, blk_data_rdreq, blk_meta_rdreq;
 
-wire [7:0] blk_data;
-wire usedw;
-wire blk_empty, blk_rdreq;
-
-fifo test_fifo(clk, fifo_w_data, blk_rdreq, reset, wrreq, blk_empty, blk_data, usedw);
-convEncoder_bs encoder(clk, reset, blk_ready, blk_size, tail_bits, blk_empty, blk_data, blk_rdreq, dOut, cOut);
+fifo test_inputdata_fifo(clk, fifo_w_data, blk_data_rdreq, reset, wrreq_data, blk_empty_data, blk_data, usedw_data);
+fifo test_inputmeta_fifo(clk, fifo_w_meta, blk_meta_rdreq, reset, wrreq_meta, blk_empty_meta, blk_meta, usedw_meta);
+convEncoder_bs encoder(clk, reset, blk_ready, blk_meta, blk_empty_data, blk_data, blk_meta_rdreq, blk_data_rdreq, dOut, cOut);
 endmodule 
