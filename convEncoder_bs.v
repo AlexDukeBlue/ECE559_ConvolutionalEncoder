a1663235ea583fd4193237ef70e30409382ae79c
Alexander Zapata <alexander.zapata@duke.edu>
2018-10-22 22:33:06 -0400

    update

--- convEncoder_bs.v
+++ convEncoder_bs.v
@@ -1,21 +1,24 @@
-module convEncoder_bs(clk, reset, blk_ready, blk_meta, blk_empty, blk_data, blk_meta_rdreq, blk_data_rdreq, dOut, cOut, computation_done, instantiate_computation, compute_enable);
+module convEncoder_bs(clk, reset, blk_ready, blk_meta, blk_empty, blk_data, blk_meta_rdreq, blk_data_rdreq, dOut, cOut, computation_done, instantiate_computation, compute_enable, counter_out, counter_mod, out_to_fifo0, out_to_fifo1, out_to_fifo2);
 	//If the input block is FIFO, not sure how we get the last six
 	input [7:0] blk_data, blk_meta;
 	input clk, reset, blk_ready, blk_empty;
+	output [12:0] counter_out;
+	output [7:0] out_to_fifo0, out_to_fifo1, out_to_fifo2;
 	output [6:0] cOut; 
-	output [2:0] dOut;
+	output [2:0] dOut, counter_mod;
 	output blk_meta_rdreq, blk_data_rdreq; 
 	output computation_done, instantiate_computation, compute_enable;
 	
 	reg c0, c1, c2, c3, c4, c5, c6, compute_enable, instantiate_computation, delay_one_cycle;
 	
-	wire [12:0] counter_out;
-	wire [2:0] counter_mod;
+	//wire [12:0] counter_out;
+	//wire [2:0] counter_mod;
+	wire [7:0] out_to_fifo0, out_to_fifo1, out_to_fifo2;
 	wire d0, d1, d2, on_last_bit_of_input, small_computation_notdone, large_computation_notdone, computation_done;
 
 	assign computation_done = (blk_meta[0]) ? !large_computation_notdone : !small_computation_notdone;
 	assign blk_meta_rdreq = instantiate_computation && !blk_empty;
-	assign blk_data_rdreq = (!delay_one_cycle && instantiate_computation && !blk_empty) || (on_last_bit_of_input && !blk_empty && compute_enable);
+	assign blk_data_rdreq = (delay_one_cycle && instantiate_computation && !blk_empty) || (on_last_bit_of_input && !blk_empty && compute_enable);
 	assign d0 = c0 ^ c2 ^ c3 ^ c5 ^ c6;
 	assign d1 = c0 ^ c1 ^ c2 ^ c3 ^ c6;
 	assign d2 = c0 ^ c1 ^ c2 ^ c4 ^ c6;
@@ -38,6 +41,7 @@ module convEncoder_bs(clk, reset, blk_ready, blk_meta, blk_empty, blk_data, blk_
 		else if(delay_one_cycle)
 		begin
 			delay_one_cycle <= 1'b0;
+			compute_enable <= 1'b1;
 		end else if(computation_done)
 		begin
 			compute_enable <= 1'b0;
@@ -73,11 +77,13 @@ module convEncoder_bs(clk, reset, blk_ready, blk_meta, blk_empty, blk_data, blk_
 				c5 <= blk_meta[3];
 				c6 <= blk_meta[2];
 				instantiate_computation <= 1'b0;
-			   compute_enable <= 1'b1;
+				//compute_enable <= 1'b1;
 			end
 		end
 	end
 
-	//Need to set up case for what to do if blk_empty is asserted when we need it to not be lmao.
+	shiftreg outreg0(clk, (compute_enable && !computation_done), reset, d0, out_to_fifo0);
+	shiftreg outreg1(clk, (compute_enable && !computation_done), reset, d1, out_to_fifo1);
+	shiftreg outreg2(clk, (compute_enable && !computation_done), reset, d2, out_to_fifo2);
 	
 endmodule
--- skeleton.v
+++ skeleton.v
@@ -1,6 +1,9 @@
-module skeleton(clk, reset, blk_ready, fifo_w_data, fifo_w_meta, blk_data, blk_meta, wrreq_data, wrreq_meta, cOut, dOut, computation_done, instantiate_computation, compute_enable);
+module skeleton(clk, reset, blk_ready, fifo_w_data, fifo_w_meta, blk_data, blk_meta, wrreq_data, wrreq_meta, cOut, dOut, computation_done, instantiate_computation, compute_enable, counter_out, counter_mod, out_to_fifo0, out_to_fifo1, out_to_fifo2);
 input [7:0] fifo_w_data, fifo_w_meta;
 input clk, reset, blk_ready, wrreq_data, wrreq_meta;
+output [12:0] counter_out;
+output [7:0] out_to_fifo0, out_to_fifo1, out_to_fifo2;
+output [2:0] counter_mod;
 output [7:0] blk_data, blk_meta;
 output [6:0] cOut;
 output [2:0] dOut;
@@ -12,5 +15,5 @@ wire blk_empty_data, blk_empty_meta, blk_data_rdreq, blk_meta_rdreq;
 
 fifo test_inputdata_fifo(clk, fifo_w_data, blk_data_rdreq, reset, wrreq_data, blk_empty_data, blk_data, usedw_data);
 fifo test_inputmeta_fifo(clk, fifo_w_meta, blk_meta_rdreq, reset, wrreq_meta, blk_empty_meta, blk_meta, usedw_meta);
-convEncoder_bs encoder(clk, reset, blk_ready, blk_meta, blk_empty_data, blk_data, blk_meta_rdreq, blk_data_rdreq, dOut, cOut, computation_done, instantiate_computation, compute_enable);
+convEncoder_bs encoder(clk, reset, blk_ready, blk_meta, blk_empty_data, blk_data, blk_meta_rdreq, blk_data_rdreq, dOut, cOut, computation_done, instantiate_computation, compute_enable, counter_out, counter_mod, out_to_fifo0, out_to_fifo1, out_to_fifo2);
 endmodule 
