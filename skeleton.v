55f38d11314db10811cbad2e304a24e105f39da6
Alexander Zapata <alexander.zapata@duke.edu>
2018-11-12 20:01:14 -0500

    work

--- skeleton.v
+++ skeleton.v
@@ -10,11 +10,11 @@ wire [7:0] blk_data, tail_byte;
 //Currently usedw_data and usedw_meta aren't being used but they are part of the FIFO interface.
 wire usedw_data, usedw_meta;
 //blk_empty is the empty signal for the data FIFO from the code-segmentation block.
-wire blk_empty, blk_data_rdreq, blk_ready, code_block_length;
+wire blk_empty, blk_data_rdreq, data_valid, code_block_length, length_out;
 
 convEncoder_bs encoder(.clk(clk),
 							  .reset(reset),
-							  .blk_ready(blk_ready),
+							  .data_valid(data_valid),
 							  .tail_byte(tail_byte),
 							  .code_block_length(code_block_length),
 							  .blk_empty(blk_empty),
@@ -24,7 +24,8 @@ convEncoder_bs encoder(.clk(clk),
 							  .q1(q1),
 							  .q2(q2),
 							  .rdreq_subblock(rdreq_subblock),
-							  .computation_done(computation_done)
+							  .computation_done(computation_done),
+							  .length_out(length_out)
 							  );
 
 endmodule 
