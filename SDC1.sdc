79a062c6670b667e457c9c3cfe480bfe4ac52d2d
joshowsiany <jpo7@duke.edu>
2018-12-02 17:54:05 -0500

    with hardware

--- SDC1.sdc
+++ SDC1.sdc
@@ -1,2 +1,2 @@
-create_clock -name clk -period 20.000 -waveform {0 10} [get_ports {clk}]
+create_clock -name clk50 -period 20.000 -waveform {0 10} [get_ports {clk50}]
 derive_clock_uncertainty
--- skeleton.v
+++ skeleton.v
@@ -1,23 +1,67 @@
-module skeleton(clk, reset, data_valid, fifo_w_data, tail_byte, wrreq_data, q0, q1, q2, rdreq_subblock, code_block_length, computation_done);
-input [7:0] fifo_w_data, tail_byte;
-input rdreq_subblock, code_block_length;
-input clk, reset, data_valid, wrreq_data;
+module skeleton(clk, clk50, out_clk, reset, blk_ready, blk_ready_out, fifo_w_data, blk_data, wrreq_data, q0, q1, q2, computation_done, test_done);
+input [7:0] fifo_w_data;
+input clk, clk50, reset, blk_ready, wrreq_data;
 output [7:0] q0, q1, q2;
-output computation_done;
+output [7:0] blk_data;
+output computation_done, out_clk;
+output reg test_done, blk_ready_out;
 
-wire [9:0] usedw0, usedw1, usedw2;
+reg [7:0] counter;
+reg computation_ended, rdreq_subblock, switch_clocks, was_ready;
+
 wire [7:0] blk_data;
-wire usedw_data, usedw_meta;
-wire blk_empty, blk_data_rdreq, length_out;
+wire usedw_data;
+wire blk_empty_data, blk_data_rdreq;
+wire ireset, iwrreq_data, length_out, actual_clock; 
 
-fifo test_inputdata_fifo(clk, fifo_w_data, blk_data_rdreq, reset, wrreq_data, blk_empty, blk_data, usedw_data);
+assign actual_clock = (switch_clocks) ? clk50 : ~clk;
+assign out_clk = actual_clock;
+assign ireset = ~reset;
+assign iwrreq_data = ~wrreq_data;
 
-convEncoder_bs encoder(.clk(clk),
-							  .reset(reset),
-							  .data_valid(data_valid),
-							  .tail_byte(tail_byte),
-							  .code_block_length(code_block_length),
-							  .blk_empty(blk_empty),
+initial begin
+	switch_clocks <= 1'b0;
+	test_done <= 1'b0;
+	computation_ended <= 1'b0;
+	blk_ready_out <= 1'b0;
+	was_ready <= 1'b0;
+	rdreq_subblock <= 1'b0;
+end
+
+always @(posedge actual_clock) begin
+	if(blk_ready_out)
+	begin
+		blk_ready_out <= 1'b0;
+	end
+	if(blk_ready && ~was_ready)
+	begin
+		blk_ready_out <= 1'b1;
+		switch_clocks <= 1'b1;
+		was_ready <= 1'b1;
+	end
+	if(computation_done)
+	begin
+		computation_ended <= 1'b1;
+	end
+	if(counter >= 8'd132)
+	begin
+		computation_ended <= 1'b0;
+		test_done <= 1'b1;
+	end
+	if(computation_ended)
+	begin
+		rdreq_subblock <= 1'b1;
+		counter <= counter + 1'b1;
+	end
+end
+
+fifo test_inputdata_fifo(actual_clock, fifo_w_data, blk_data_rdreq, ireset, iwrreq_data, blk_empty_data, blk_data, usedw_data);
+convEncoder_bs encoder(.clk(actual_clock),
+							  .reset(ireset),
+							  .data_valid(blk_ready_out),
+							  .tail_byte(8'd0),
+							  .code_block_length(1'b0),
+							  .blk_empty(blk_empty_data),
 							  .blk_data(blk_data),
 							  .blk_data_rdreq(blk_data_rdreq),
 							  .q0(q0),
