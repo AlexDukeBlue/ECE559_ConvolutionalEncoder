9c717d47050d94028ac62cdaf65efabcc93a95fb
Alexander Zapata <alexander.zapata@duke.edu>
2018-12-14 14:01:40 -0500

    Update seven_segment.v

--- seven_segment.v
+++ seven_segment.v
@@ -1,22 +1,21 @@
-module seven_segment(inPins, outPins);
-input 	[3:0]	inPins;
-output 	[6:0] outPins;
+module seven_segment(in, out);
+input 	[3:0]	in;
+output 	[6:0]  out;
 
-assign outPins =	(inPins==4'h0)?7'b1000000:
-					(inPins==4'h1)?7'b1111001:
-					(inPins==4'h2)?7'b0100100:
-					(inPins==4'h3)?7'b0110000:
-					(inPins==4'h4)?7'b0011001:
-					(inPins==4'h5)?7'b0010010:
-					(inPins==4'h6)?7'b0000010:
-					(inPins==4'h7)?7'b1111000:
-					(inPins==4'h8)?7'b0000000:
-					(inPins==4'h9)?7'b0010000:
-					(inPins==4'hA)?7'b0001000:
-					(inPins==4'hB)?7'b0000011:
-					(inPins==4'hC)?7'b1000110:
-					(inPins==4'hD)?7'b0100001:
-					(inPins==4'hE)?7'b0000110:
-					(inPins==4'hF)?7'b0001110:7'b0;
-
+	assign out = (in == 4'b0000) ? 7'b1000000:
+		     (in == 4'b0001) ? 7'b1111001 :
+		     (in == 4'b0010) ? 7'b0100100 :
+		     (in == 4'b0011) ? 7'b0110000 :
+		     (in == 4'b0100) ? 7'b0011001 :
+		     (in == 4'b0101) ? 7'b0010010 :
+		     (in == 4'b0110) ? 7'b0000010 :
+		     (in == 4'b0111) ? 7'b1111000 :
+		     (in == 4'b1000) ? 7'b0000000 :
+		     (in == 4'b1001) ? 7'b0010000 :
+		     (in == 4'b1010) ? 7'b0001000 :
+		     (in == 4'b1011) ? 7'b0000011 :
+		     (in == 4'b1100) ? 7'b1000110 :
+		     (in == 4'b1101) ? 7'b0100001 :
+		     (in == 4'b1110) ? 7'b0000110 :
+		     (in == 4'b1111) ? 7'b0001110 : 7'b0;
 endmodule
